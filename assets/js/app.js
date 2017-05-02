f7f97c416c7300e25a91dcf4b227363b948aae2b
Betsi Loayza <betsi18_12@hotmail.com>
2017-05-02 17:52:25 -0500

    falta incluir despliegue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 var enviar = document.getElementById("button1");
 var celular = document.getElementById("numero");
+var oculto = document.getElementById("ocultar");
 window.addEventListener("load", function (e){
   e.preventDefault();
-
   enviar.addEventListener("click", function(){
     if(celular.value == "" || /[9]{1}[0-9]{8}/.test(celular.value)){
         alert("Escriba su numero correcto");
